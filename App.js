28b5398f05ac9e55c056c44107fdae718b19b71c
iknowmagic <ilan@iknowmagic.net>
2018-12-01 15:13:01 -0800

    init app

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Vibration, Button } from 'react-native';
 
 export default class App extends React.Component {
+  initApp() {
+  }
+  StartExplorer() {
+    Vibration.vibrate([250, 10000, 250, 2000, 250, 250, 250, 250])
+  }
+  componentDidMount() {
+    // Vibration.vibrate([250, 250])
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <Button title="Start Dance Explorer" onPress={this.StartExplorer} />
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
